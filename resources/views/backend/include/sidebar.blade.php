a510408162da14b2a4df0831c8ff55f32c47f660
alhassan <alhassan.cse@gmail.com>
2024-04-30 01:24:05 +0600

    fixed

--- resources/views/backend/include/sidebar.blade.php
+++ resources/views/backend/include/sidebar.blade.php
@@ -21,7 +21,7 @@
 
     <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
-        <li class="active treeview menu-open">
+        <li>
             <a href="{{ route('home') }}">
                 <i class="fa fa-dashboard"></i> <span>Dashboard</span>
             </a>
